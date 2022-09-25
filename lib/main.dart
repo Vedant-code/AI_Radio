a5a3bb929f7a9312d546a6b3a3e31c9314b2d087
Vedant-code <vedant.dhote20@pccoepune.org>
2022-09-25 11:52:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+// @dart=2.9 
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:radio/pages/home_pge.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
+      ),
+      home: HomePage(),
+    );
+  }
+}
